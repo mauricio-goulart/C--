d318611198c106bee9f75fcd5718bce24c53983f
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-06-26 23:02:00 -0300

    Create main.cpp

---
+++ Aulas/1 - Variaveis e entradas/main.cpp
@@ -0,0 +1,27 @@
+/*
+Variaveis e entradas
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    float altura;
+    int cep;
+    int idade = 20;
+    char sexualidade = 'h';
+    string nome = "mauricio";
+
+    altura = 1.70;
+    
+    cout << "Qual o CEP = ";
+    cin >> cep;
+    
+    cout << "Nome = " << nome << endl << "Idade = " << idade << endl << "Altura = " << altura << endl << "Sexualidade = " << sexualidade << endl << "CEP = " << cep << endl;
+
+    return 0;
+
+}
