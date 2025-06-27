0167a1c0fd2af4e86e7b5519b0b96611e7b55932
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-06-26 23:01:56 -0300

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+/*
+    Primeiros comandos
+*/
+
+#include <iostream>
+
+int main()
+{
+
+  //std::cout << "Hello World!" << std::endl;
+
+    std::cout << "Hello World!\nMauricio\n";
+
+  //system("pause") - só funciona em Windows; 
+
+    return 0;
+}
