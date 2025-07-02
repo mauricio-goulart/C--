29a9ea4c8f50a89bf4e619dcd850fcbdf2bc4aae
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-01 23:05:00 -0300

    Create main.cpp

---
+++ Aulas/5 - Estrutura de Repetição/main.cpp
@@ -0,0 +1,38 @@
+/*
+Estrutura de repetições
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, s = 0;
+
+    cout << "n = ";
+    cin >> n;
+
+    while (n <= 0)
+    {
+        cout << "Digite um numero diferente de 0 e positivo! = ";
+        cin >> n;
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        s = s + i;
+    }
+
+    cout << s << endl;
+
+
+    
+
+
+    
+    
+
+
+    return 0;
+}
