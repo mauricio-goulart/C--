392a1d4953b720395659a33c461600267ba59590
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-09 23:41:46 -0300

    Create atv4.cpp

---
+++ Desafios/Nivel 1/atv4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    string cond;
+    int n1, n2;
+
+    do
+    {
+        int x;
+
+        cout << "base = ";
+        cin >> n1;
+
+        cout << "expoente = ";
+        cin >> n2;
+
+        x = pow(n1, n2);
+        
+        cout << "Valor = " << x << endl;
+
+        cout << "Quer repetir?: ";
+        cin >> cond;
+
+
+
+    } while (cond == "sim");
+    
+
+    return 0;
+}
