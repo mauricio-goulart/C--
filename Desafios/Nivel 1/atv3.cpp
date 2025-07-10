31fc265129891f9c5eec4a0f9c58357c2c526fff
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-09 23:08:18 -0300

    Create atv3.cpp

---
+++ Desafios/Nivel 1/atv3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int fat(int num)
+{
+    int tot = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        tot = tot * i;
+    }
+
+    return tot;
+    
+}
+
+int main()
+{
+    int n, fatorial;
+
+    cout << "Digite um numero: ";
+    cin >> n;
+
+    fatorial = fat(n);
+
+    cout << "Fatorial de [" << n << "] = [" << fatorial << "]" << endl;
+
+    return 0;
+}
