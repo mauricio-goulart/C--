8d4f0ad54bf1d7f7d6b8ee2014b785ce7a0c1925
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-29 16:31:27 -0300

    Update main.cpp

--- Aulas/6 - Ponteiros/main.cpp
+++ Aulas/6 - Ponteiros/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     int var1;
     int* p;
-
+    
     p = NULL;
 
     var1 = 5;
@@ -21,13 +21,16 @@ int main()
     cout << p << endl; // Valor do endereço da variavel
 
     cout << *p << endl; // Valor da variavel
-
+    
     *p = 45; // Altera o valor da varivel
-
+    
     int* p2 = new int;
-
+    
     *p2 = 35;
-
+    
+    //delete p2; para não vazar memoria
+    p2 = p; // Está apontando para onde o p aponta, ocasionando uma fuga de memoria
+    
     return 0;
 
 }
