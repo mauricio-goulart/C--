17dfbeb380ee2b4ac658fbf3b29aa76d78516f86
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-29 16:15:17 -0300

    Update main.cpp

--- Aulas/6 - Ponteiros/main.cpp
+++ Aulas/6 - Ponteiros/main.cpp
@@ -3,6 +3,7 @@ Ponteiros
 */
 
 #include <iostream>
+#include <cstddef>
 
 using namespace std;
 
@@ -12,11 +13,20 @@ int main()
     int var1;
     int* p;
 
+    p = NULL;
+
     var1 = 5;
     p = &var1; // Ponteiro está apontando para a variavel
 
     cout << p << endl; // Valor do endereço da variavel
 
+    cout << *p << endl; // Valor da variavel
+
+    *p = 45; // Altera o valor da varivel
+
+    int* p2 = new int;
+
+    *p2 = 35;
 
     return 0;
 
