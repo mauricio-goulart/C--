ef6402f5268db13d65669a86fe8be3f9074b4d0c
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-29 16:43:31 -0300

    Update main.cpp

---
+++ Aulas/7 - Vetores/main.cpp
@@ -0,0 +1,24 @@
+/*
+Vetores
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int vet[4];
+    int vet2[] = {1,2,3};
+    
+    vet[0] = 20;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        cout << vet2[i] << " ";
+    }
+    
+    return 0;
+    
+}
