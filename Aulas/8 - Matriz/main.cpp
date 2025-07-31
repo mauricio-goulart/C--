1c4d3a4e01d1247c74889f18a09bc875c5d7c056
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-31 19:00:31 -0300

    Update main.cpp

--- Aulas/8 - Matriz/main.cpp
+++ Aulas/8 - Matriz/main.cpp
@@ -1,1 +1,30 @@
+/*
+Matrizes
+*/
+
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int matriz[3][4] = {
+        {2,3,4,6},
+        {5,6,7,2},
+        {6,3,4,9}
+    } ;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << matriz[i][j] << " ";
+        }
+
+        cout << endl;
+        
+    }
+    
+    return 0;
+}
