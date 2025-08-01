7b44a4b612d3f1dba8caf51df2fc1256d104a792
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-31 21:54:19 -0300

    Update atv4.cpp

--- Desafios/Nivel 2/atv4.cpp
+++ Desafios/Nivel 2/atv4.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+    int* p1 = new int;
+    int* p2 = new int;
+    float* media = new float;
+    
+    cout << "Idade: ";
+    cin >> *p1;
 
+    cout << "Idade: ";
+    cin >> *p2;
+
+    *media = (*p1 + *p2)/ 2;
+
+    cout << "media = " << *media;
 }
