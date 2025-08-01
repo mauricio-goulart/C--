e1c9b2d8c53996aa96f8d5f39128e78869681aa4
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-31 21:21:58 -0300

    Add matrix input and sum program
    Creates a C++ program that reads matrix dimensions and values from user input, displays the matrix, and outputs the sum of all its elements.

---
+++ Desafios/Nivel 2/atv2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int* n = new int[2];
+    int soma = 0;
+
+    cout << "Digite dois numeros = [][] ";
+    cin >> n[0];
+    cin >> n[1];
+
+    int mat[n[0]][n[1]];
+
+    for (int i = 0; i < n[0]; i++)
+    {
+        for (int j = 0; j < n[1]; j++)
+        {
+            cout << "[" << i + 1 << "]" << "[" << j + 1 << "] = ";
+            cin >> mat[i][j];
+        }
+        
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < n[0]; i++)
+    {
+        for (int j = 0; j < n[1]; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+
+        cout << endl;
+        
+    }
+
+    for (int i = 0; i < n[0]; i++)
+    {
+        for (int j = 0; j < n[1]; j++)
+        {
+            soma = soma + mat[i][j];
+        }
+
+    }
+    
+    cout << "Soma = " << soma << endl;
+    
+        
+}
