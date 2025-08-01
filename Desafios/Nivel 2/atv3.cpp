3adcb4009d8b9812de384f95d089e4a7f687f846
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-07-31 21:37:57 -0300

    Create atv3.cpp

---
+++ Desafios/Nivel 2/atv3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1;
+    string cond;
+
+    do
+    {
+        
+        cout << "Digite o tamanho do vetor = ";
+        cin >> n1;
+
+        int* vet = new int[n1];
+
+        for (int i = 0; i < n1; i++)
+        {
+            cout << "[" << i + 1 << "] = ";
+            cin >> vet[i];
+        }
+        
+        cout << "Deseja continuar? (sim/nao) ";
+        cin >> cond;
+
+    } while (cond == "sim");
+    
+    return 0;
+
+}
