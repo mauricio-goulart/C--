81335837e082f6892d400d499a5c9f15aeb6bfd3
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-08-14 15:40:52 -0300

    Update main.cpp

--- Aed/Desafios/main.cpp
+++ Aed/Desafios/main.cpp
@@ -8,86 +8,103 @@ Faça um programa que calcule operacoes e guarde na pilha
 
 using namespace std;
 
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
 int main()
 {
     stack my_stack;
     type_item item;
     int menu_option;
 
+    int n1,n2;
+
+
     do
     {
+        cout << "-------- Menu Multiply --------" << endl;
+        cout << "Number 1: ";
+        cin >> n1;
+        cout << "Number 2: ";
+        cin >> n2;
 
-        cout << "-------- Menu Stack --------" << endl;
-        cout << "[1] - Push " << endl;
-        cout << "[2] - Pop " << endl;
-        cout << "[3] - Clear " << endl;
-        cout << "[4] - Top Item " << endl;
-        cout << "[5] - Bottom Item " << endl;
-        cout << "[6] - Print " << endl;
-        cout << "[7] - Search " << endl;
-        cout << "[-1] - Exit " << endl << endl;
-        cout << "----------------------------" << endl << endl;
-        cout << "Option: ";
-        cin >> menu_option;
-  
-        switch (menu_option)
+        int mult = multiply(n1, n2);
+
+        do
         {
+            cout << "-------- Menu Stack --------" << endl;
+            cout << "[1] - Push " << endl;
+            cout << "[2] - Pop " << endl;
+            cout << "[3] - Clear " << endl;
+            cout << "[4] - Top Item " << endl;
+            cout << "[5] - Bottom Item " << endl;
+            cout << "[6] - Print " << endl;
+            cout << "[7] - Search " << endl;
+            cout << "[0] - New Number" << endl;
+            cout << "[-1] - Exit " << endl << endl;
+            cout << "----------------------------" << endl << endl;
+            cout << "Option: ";
+            cin >> menu_option;
+    
+            switch (menu_option)
+            {
 
-        case 1:
+            case 1:
 
-            cout << "Insert Item: ";
-            cin >> item;
-            my_stack.push(item);
+                my_stack.push(mult);
 
-            break;
-        
-        case 2:
-
-            item = my_stack.pop();
-            cout << "Removed: " << item << endl;
-
-            break;
-
-        case 3:
-
-            my_stack.clear();
-
-            break;
-
-        case 4:
-
-            item = my_stack.top();
-            cout << "Top: " << item << endl;
-
-            break;
-
-        case 5:
-
-            item = my_stack.bottom();
-            cout << "Bottom: " << item << endl;
-
-            break;
-
-        case 6:
+                break;
             
-            my_stack.print();
+            case 2:
 
-            break;
+                item = my_stack.pop();
+                cout << "Removed: " << item << endl;
 
-        case 7:
+                break;
 
-            int index;
-            cout << "Search: ";
-            cin >> item;
+            case 3:
 
-            index = my_stack.search(item);
+                my_stack.clear();
 
-            cout << "Index: " << indice << endl;
+                break;
 
-        default:
-            break;
+            case 4:
 
-        }
+                item = my_stack.top();
+                cout << "Top: " << item << endl;
+
+                break;
+
+            case 5:
+
+                item = my_stack.bottom();
+                cout << "Bottom: " << item << endl;
+
+                break;
+
+            case 6:
+                
+                my_stack.print();
+
+                break;
+
+            case 7:
+
+                int index;
+                cout << "Search: ";
+                cin >> item;
+
+                index = my_stack.search(item);
+
+                cout << "Index: " << index << endl;
+
+            default:
+                break;
+
+            }
+        } while (menu_option != 0 && menu_option != -1);
 
     } while (menu_option != -1);
     
