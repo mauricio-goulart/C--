ccb59d08cd95a70f0a570d95709496ad2fc074d5
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-08-14 14:40:32 -0300

    Update main.cpp

--- Aed/Desafios/main.cpp
+++ Aed/Desafios/main.cpp
@@ -11,6 +11,59 @@ using namespace std;
 int main()
 {
     stack my_stack;
+    type_item item;
+    int menu_option;
+
+    do
+    {
+
+        cout << " -------- Menu Stack -------- " << endl;
+        cout << "[1] - Push " << endl;
+        cout << "[2] - Pop " << endl;
+        cout << "[3] - Clear " << endl;
+        cout << "[4] - Top Item " << endl;
+        cout << "[5] - Peak Item " << endl;
+        cout << "[6] - Print " << endl;
+        cout << "[7] - Search " << endl;
+        cout << "[-1] - Exit " << endl << endl;
+        cout << "Option: ";
+        cin >> menu_option;
+
+        cout << endl << " ---------------------------- " << endl << endl;
+
+
+
+        switch (menu_option)
+        {
+        case 1:
+
+            cout << "Insert Item: ";
+            cin >> item;
+            my_stack.push(item);
+
+            break;
+        
+        case 2:
+
+            break;
+        
+            
+        default:
+            break;
+        }
+
+
+
+        
+
+
+
+
+
+
+    } while (menu_option != -1);
+    
+
 
 
     return 0;
