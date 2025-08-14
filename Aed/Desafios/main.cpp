06bd26c72b446fce10c8d8aacb9e2ed215f3ecae
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-08-13 23:41:32 -0300

    Update main.cpp

--- Aed/Desafios/main.cpp
+++ Aed/Desafios/main.cpp
@@ -4,12 +4,13 @@ Faça um programa que calcule operacoes e guarde na pilha
 
 */
 #include <iostream>
+#include "pilha.h"
 
-using namespace namespace std;
+using namespace std;
 
 int main()
 {
-
+    stack my_stack;
 
 
     return 0;
