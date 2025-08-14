cdd8dd1d44444978b9b5c9f82cf26fe0c93b64d2
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2025-08-13 21:51:46 -0300

    Update pilha.h

--- Aed/Desafios/pilha.h
+++ Aed/Desafios/pilha.h
@@ -1,15 +1,31 @@
-typedef int tipo_item;
-using namespace std;
+typedef int type_item;
+const int max_items = 100;
 
-class pilha
+class stack
 {
     private:
 
+        int size;
+        int* structure = new int[max_items];
 
     public:
 
+        stack();
+        ~stack();
 
+        void push(type_item item);
+        type_item pop();
+        void clear();
 
+        void top();
+        void peak();
+
+        int size();
+        void print();
+        void search(type_item item);
+
+        bool is_full();
+        bool is_empty();
 
 
 };
